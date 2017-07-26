ad30948f7e451243d6e9e00d544c587b89e9bfa6
Unknown <geoff.w.wilkinson@gmail.com>
2017-07-26 21:56:20 +0700

    Bug fix for 1 year old

--- JS intro/script.js
+++ JS intro/script.js
@@ -1,7 +1,7 @@
 var firstName = prompt("What is your first name?");
 var lastName = prompt("What is your last name?");
 var age = prompt("How old are you?");
-function plural() { if(age != 1) {return "s";} }
+function plural() { if(age != 1) {return "s";} return "";}
 
 console.log("Your full name is " + firstName + " " + lastName + ".");
 console.log("Your are " + age + " year" + plural() + " old.");
