adf921a0ea998fac52a3b7effada90de6ec7c28c
Unknown <geoff.w.wilkinson@gmail.com>
2017-07-28 16:21:18 +0700

    Scorekeeper finished

--- score keeper/code.js
+++ score keeper/code.js
@@ -2,6 +2,8 @@ var p1Button = document.querySelector("#p1");
 var p2Button = document.querySelector("#p2");
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
+var numInput = document.querySelector("input");
+var winningScoreDisplay = document.querySelector("#winningScoreDisplay");
 var resetButton = document.querySelector("#reset");
 
 var gameOver = false;
@@ -11,6 +13,7 @@ var p2Score = 0;
 
 p1Button.addEventListener("click", scoreUpP1);
 p2Button.addEventListener("click", scoreUpP2);
+numInput.addEventListener("change", setWinningScore);
 resetButton.addEventListener("click", resetGame);
 
 function scoreUpP1() {
@@ -36,13 +39,17 @@ function scoreUpP2() {
 }
 
 function resetGame() {
-	if(gameOver) {
-		p1Score = 0;
-		p1Display.textContent = p1Score;
-		p2Score = 0;
-		p2Display.textContent = p2Score;
-		gameOver = false;
-		p1Display.classList.remove("winner");
-		p2Display.classList.remove("winner");
-	}
+	p1Score = 0;
+	p1Display.textContent = p1Score;
+	p2Score = 0;
+	p2Display.textContent = p2Score;
+	gameOver = false;
+	p1Display.classList.remove("winner");
+	p2Display.classList.remove("winner");
+}
+
+function setWinningScore() {
+	winningScoreDisplay.textContent = numInput.value;
+	winningScore = Number(numInput.value);
+	resetGame();
 }
